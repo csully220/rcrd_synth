c07168ca76ba6c1fd57b58dd52093819a4eff3d8
csully220 <colinsully@msn.com>
2017-10-01 01:02:39 +0000

    gitignores

--- raspi/old/play_note.py
+++
@@ -1,8 +0,0 @@
-from mido import MidiFile
-
-mid = MidiFile('song.mid')
-
-for i, track in enumerate(mid.tracks):
-    print('Track {}: {}'.format(i, track.name))
-    for msg in track:
-        print(msg.type)
